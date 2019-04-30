f960765a2bdb85db654a24a3cb88bf0725805211
FunkyMax <max-bauer97@web.de>
2019-04-30 18:21:58 +0200

    intensität und winkel werden pro frame geloggt.

--- app/src/main/java/com/example/catchmeifyoucan/GameController.kt
+++ app/src/main/java/com/example/catchmeifyoucan/GameController.kt
@@ -3,20 +3,30 @@ package com.example.catchmeifyoucan
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.widget.ImageView
+import android.widget.TextView
+import io.github.controlwear.virtual.joystick.android.JoystickView
 import kotlin.random.Random
 
 class GameController{
-    var duration : Long = 700
-    var randomX : Random = Random
-    var randomY : Random = Random
+    private val duration : Long = 700
+    private var random : Random = Random
+    var pace = 0
 
-    fun moveBlackBallRandomly(image: ImageView){
-        val animationX : ObjectAnimator  = ObjectAnimator.ofFloat(image, "x", randomX.nextFloat()*1000)
-        val animationY : ObjectAnimator  = ObjectAnimator.ofFloat(image, "y", randomY.nextFloat()*2700)
+    fun moveBlackBallRandomly(blackBall: ImageView){
+        val animationX : ObjectAnimator  = ObjectAnimator.ofFloat(blackBall, "x", random.nextFloat()*1000)
+        val animationY : ObjectAnimator  = ObjectAnimator.ofFloat(blackBall, "y", random.nextFloat()*2700)
 
         val animatorSet = AnimatorSet()
         animatorSet.playTogether(animationX,animationY)
         animatorSet.duration = duration
         animatorSet.start()
     }
+
+    fun moveGreenBallWithJoystick(joystick: JoystickView, greenBall : ImageView, angleID: TextView, strengthID: TextView) {
+        joystick.setOnMoveListener { angle, strength ->
+            angleID.text = "angle: " + angle.toString()
+            strengthID.text = "strength: " + strength.toString()
+
+        }
     }
+}
--- app/src/main/java/com/example/catchmeifyoucan/MainActivity.kt
+++ app/src/main/java/com/example/catchmeifyoucan/MainActivity.kt
@@ -10,28 +10,37 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity(){
-    val handler = Handler()
     val gameController = GameController()
-
-
-    private val runnable = object : Runnable {
-        override fun run() {
-            gameController.moveBlackBallRandomly(blackBall)
-            handler.postDelayed(this, 700)
-        }
-    }
+    val blackBallHandler = Handler()
+    val greenBallHandler = Handler()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         setContentView(R.layout.activity_main)
+        println("test")
         val joystick = findViewById<JoystickView>(R.id.joystickView)
-        joystick.setOnMoveListener { angle, strength ->
-            angleID.text = "angle: " + angle
-            strengthID.text = "strength: " + strength
-        }
-        //joystick.alpha = 0f
-        runnable.run()
+        joystick.alpha = .35f
+        blackBallRunnable.run()
+        greenBallRunnable.run()
     }
+
+    private val blackBallRunnable = object : Runnable {
+        override fun run() {
+            gameController.moveBlackBallRandomly(blackBall)
+            blackBallHandler.postDelayed(this, 700)
+        }
+    }
+
+    private val greenBallRunnable = object : Runnable {
+        override fun run() {
+            gameController.moveGreenBallWithJoystick(findViewById(R.id.joystickView), findViewById(R.id.angleID),findViewById(R.id.strengthID))
+            greenBallHandler.postDelayed(this, 17)
+        }
+    }
+
+
+
+
 }
