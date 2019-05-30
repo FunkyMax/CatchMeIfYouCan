12a372fda0fadd0f1b122deb9b14554ad90019dc
FunkyMax <max-bauer97@web.de>
2019-05-30 15:39:02 +0200

    Arduino Code geupdatet

--- arduino/BluetoothLED/BluetoothLED.ino
+++ arduino/BluetoothLED/BluetoothLED.ino
@@ -12,22 +12,25 @@ void setup() {
   digitalWrite(LED_BUILTIN, LOW);
 
   // Die Ein- und Ausschaltwerte für die arduinointerne LED sind im UTF8 Format, da die Bluetooth LE Datenübertragung von Android zum Bluetooth Modul nur Werte in Form eines ByteArrays nach UTF Standard annimmt.
-  ON = "49";  // Ursprünglicher Wert in Android Studio zum Einschalten der LED war "1", dessen Wert in der UTF8 Tabelle 49 ist.
-  OFF = "48"; // Ursprünglicher Wert in Android Studio zum Ausschalten der LED war "0", dessen Wert in der UTF8 Tabelle 48 ist.
+  ON = "111110";  // Ursprünglicher Wert in Android Studio zum Einschalten der LED war "on", dessen Wert in der UTF8 Tabelle 49 ist.
+  OFF = "111102102"; // Ursprünglicher Wert in Android Studio zum Ausschalten der LED war "off", dessen Wert in der UTF8 Tabelle 48 ist.
 }
 
 void loop() {
   // Wenn Daten vom Bluetooth Modul kommen, printe sie im seriellen Monitor und leite sie weiter an die arduinointerne LED
   if (BTSerial.available()) {
-    data = BTSerial.read();
-    Serial.print(data);
-    
+    data += BTSerial.read();
+    Serial.print(data + " ");
+    //Serial.println("");
+
     if (data.equals(ON)){
       digitalWrite(LED_BUILTIN, HIGH);
+      data = "";
     }
-    
+
     else if (data.equals(OFF)){
       digitalWrite(LED_BUILTIN, LOW);
+      data = "";
     }
   }
 }
